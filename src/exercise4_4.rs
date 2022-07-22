4945ac064d8ea07a26ff4dc27d125638b89bfefc
Yingbo Li <liveitbe@126.com>
2022-07-22 17:01:18 +0800

    graph parsing; resolve cyclic reference with raw pointer;

--- src/exercise4_4.rs
+++ src/exercise4_4.rs
@@ -1,101 +1,87 @@
 use std::collections::BTreeMap;
 
-struct GraphPrototype<'a> {
-    pub state: Vec<StatePrototype<'a>>,
-    pub action: Vec<ActionPrototype<'a>>,
-    pub transition: Vec<TransitionPrototype>,
-}
-
-struct GraphLookup<'a> {
-    pub state: BTreeMap<&'a str, usize>,
-    pub action: BTreeMap<&'a str, usize>,
-}
-
-struct StatePrototype<'a> {
-    pub name: &'a str,
-    pub reward: i32,
-    pub index: usize,
-}
-
-struct ActionPrototype<'a> {
-    pub name: &'a str,
-    pub reward: i32,
-    pub index: usize,
-}
-
-struct TransitionPrototype {
-    pub action: usize,
-    pub from: usize,
-    pub to: usize,
-    pub prob: f32,
-}
+//for cyclic reference:
+//https://eli.thegreenplace.net/2021/rust-data-structures-with-circular-references/
 
 struct Graph<'a> {
     pub state: Vec<State<'a>>,
+    pub action: Vec<Action<'a>>,
+    pub state_lookup: BTreeMap<&'a str, *mut State<'a>>,
+    pub action_lookup: BTreeMap<&'a str, *const Action<'a>>,
     pub policy_v: f32,
     pub state_v: Vec<f32>,
 }
 
 struct State<'a> {
-    pub proto: &'a StatePrototype<'a>,
-    pub action_list: Vec<Action<'a>>,
+    pub name: &'a str,
+    pub reward: i32,
+    pub action: Vec<Transition<'a>>,
 }
 
 struct Action<'a> {
-    pub proto: &'a ActionPrototype<'a>,
+    pub name: &'a str,
+    pub reward: i32,
+}
+
+struct Transition<'a> {
+    pub action: &'a Action<'a>,
+    pub from: &'a State<'a>,
     pub to: &'a State<'a>,
     pub prob: f32,
 }
 
-impl<'a> State<'a> {
-    fn new(proto:&'a StatePrototype) -> Self {
-        Self { proto, action_list: Vec::new() }
-    }
-}
-
-impl<'a> Action<'a> {
-    fn new(proto:&'a ActionPrototype, to:&'a State, prob:f32) -> Self {
-        Self { proto, to, prob }
-    }
-}
-
-impl<'a> GraphPrototype<'a> {
+impl<'a> Graph<'a> {
     fn new() -> Self {
         Self {
-            state: Vec::new(),
-            action: Vec::new(),
-            transition: Vec::new(),
+            state: Vec::new(), state_lookup: BTreeMap::new(),
+            action: Vec::new(), action_lookup: BTreeMap::new(),
+            policy_v: 0.0, state_v: Vec::new(),
         }
     }
 
     fn add_state(&mut self, name:&'a str, reward:i32) {
-        let state = StatePrototype { name, reward, index: self.state.len() };
+        let state = State { name, reward, action: Vec::new() };
         self.state.push(state);
+        self.state_lookup.insert(name, self.state.last_mut().unwrap());
     }
 
     fn add_action(&mut self, name:&'a str, reward:i32) {
-        let action = ActionPrototype { name, reward, index: self.action.len() };
+        let action = Action { name, reward };
         self.action.push(action);
+        self.action_lookup.insert(name, self.action.last().unwrap());
     }
 
-    fn add_transition(&mut self, lookup: &GraphLookup, action:&str, from:&str, to:&str, prob:f32) {
-        let action = lookup.action[action];
-        let from = lookup.state[from];
-        let to = lookup.state[to];
-        self.transition.push(TransitionPrototype { action, from, to, prob });
+    fn add_transition(&self, action:&str, from:&'a str, to:&str, prob:f32) {
+        if !self.action_lookup.contains_key(action)
+            || !self.state_lookup.contains_key(from)
+            || !self.state_lookup.contains_key(to) {
+            println!("invalid transition {:?}:{:?}->{:?}", action, from, to);
+            return
+        }
+        unsafe {
+            let action = &(*self.action_lookup[action]);
+            let from = self.state_lookup[from];
+            let to = &(*self.state_lookup[to]);
+            (*from).action.push(Transition::<'a> { action, from: &(*from), to, prob });
+        }
     }
-}
 
-impl<'a> GraphLookup<'a> {
-    fn new(proto:&GraphPrototype<'a>) -> Self {
-        Self {
-            state: proto.state.iter().map(|s| (s.name, s.index)).collect(),
-            action: BTreeMap::new()
+    fn print(&self) {
+        println!("action:");
+        for a in self.action.iter() {
+            println!("\t{:?}:{:?}", a.name, a.reward);
+        }
+        println!("state:");
+        for s in self.state.iter() {
+            println!("\t{:?}:{:?}", s.name, s.reward);
+            for t in s.action.iter() {
+                println!("\t\t{:?}:{:?}->{:?}|{:?}", t.action.name, t.from.name, t.to.name, t.prob);
+            }
         }
     }
 }
 
-fn parse_graph_prototype<'a>() -> GraphPrototype<'a> {
+fn parse_graph<'a>() -> Graph<'a> {
     // let s = "
     //     stay:0
     //     move:-4
@@ -106,29 +92,16 @@ fn parse_graph_prototype<'a>() -> GraphPrototype<'a> {
     //     >stay,s1,0.5,0
     //     >move,s0,0.5,0
     // ";
-    let mut g = GraphPrototype::new();
+    let mut g = Graph::new();
     //TODO parse from text
     g.add_state("s0", 0);
     g.add_state("s1", 0);
     g.add_action("stay", 0);
     g.add_action("move", -4);
-    let l = GraphLookup::new(&g);
-    g.add_transition(&l, "stay", "s0", "s0", 0.5);
-    g.add_transition(&l, "move", "s0", "s1", 0.5);
-    g.add_transition(&l, "stay", "s1", "s1", 0.5);
-    g.add_transition(&l, "move", "s1", "s0", 0.5);
-    g
-}
-
-fn setup_graph<'a>(gp:&'a GraphPrototype) -> Graph<'a> {
-    let state:Vec<State> = gp.state.iter()
-        .map(|p| State::new(p))
-        .collect();
-    let mut g = Graph { state, policy_v: 0.0, state_v: Vec::new() };
-    for t in gp.transition.iter() {
-        let a = Action::new(&gp.action[t.action], &g.state[t.to], t.prob);
-        g.state[t.from].action_list.push(a);
-    }
+    g.add_transition("stay", "s0", "s0", 0.5);
+    g.add_transition("move", "s0", "s1", 0.5);
+    g.add_transition("stay", "s1", "s1", 0.5);
+    g.add_transition("move", "s1", "s0", 0.5);
     g
 }
 
@@ -153,6 +126,6 @@ fn reward() {
 }
 
 pub fn run() {
-    let gProto = parse_graph_prototype();
-    let g = setup_graph(&gProto);
+    let g = parse_graph();
+    g.print();
 }
